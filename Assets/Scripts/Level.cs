c397e04183fb48638318dfeceec26b09ff49c9d0
Hasan Jafri <hasan.jafri7@gmail.com>
2018-11-01 02:34:53 -0400

    Add logic for winning level and loading next, WIP: gameSpeed changes

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -6,8 +6,25 @@ public class Level : MonoBehaviour {
 
     [SerializeField] int breakableBlocks; // Serialized for debugging purposes
 
+    //Cached references
+    SceneLoader sceneLoader;
+
     public void CountBreakableBlocks()
     {
         breakableBlocks++;
+    }
+
+    private void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+    public void BlockDestroyed()
+    {
+        breakableBlocks--;
+        if (breakableBlocks <= 0)
+        {
+            sceneLoader.LoadNextScene();
+        }
     }
 }
